20c176a188fa06b5217b92549b8c6a68a7743e66
Dan Örjestad <danorjest@gmail.com>
2021-02-08 16:15:00 +0100

    populate the category store

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import Category from './controllers/category';
-</script>
+	// Populate categories store
+	// TODO: move this to a more suitable place?
+	import category from './controllers/category';
+	import { categoryStore } from './stores/categoryStore';
 
-:)
+	category.getCategories().then((categories) => {
+		categoryStore.set(categories);
+	});
+</script>
