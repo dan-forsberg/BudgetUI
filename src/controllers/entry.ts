82b1fa4cda2a810fe16bb51daf36fda0bbd5560c
Dan Örjestad <danorjest@gmail.com>
2021-02-08 16:14:30 +0100

    implemented well enough

--- src/controllers/entry.ts
+++ src/controllers/entry.ts
@@ -6,8 +6,43 @@ const getAllEntries = async (): Promise<IEntry[]> => {
 	const response = await get("/entry");
 	const json = await response.json();
 
-	console.log(json);
+	if (response.status !== 200) {
+		throw new Error(json.message);
+	}
+
+	return json;
+};
+
+type GetAttributes = {
+	date?: Date;
+	description?: string;
+	amount?: number;
+	category?: number;
+};
+const getSpecificEntries = async (attributes: GetAttributes): Promise<IEntry[]> => {
+	/* TODO, implement */
+	//shush up eslint
+	if (attributes)
+		attributes = null;
+
 	return null;
 };
 
-export default { getAllEntries };
+const newEntry = async (newEntry: IEntry | IEntry[]): Promise<IEntry> => {
+	// the entries object has to be an array for the API
+	let body;
+	if (Array.isArray(newEntry))
+		body = { entries: newEntry };
+	else
+		body = { entries: [newEntry] };
+
+	const result = await post("/entry/new", body);
+	const json = await result.json();
+
+	if (result.status !== 201) {
+		throw new Error(json.message);
+	}
+	return json;
+};
+
+export default { getAllEntries, getSpecificEntries, newEntry };
