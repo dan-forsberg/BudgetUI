af52f146307a146bf04343093311024bc8855ea5
Dan Örjestad <danorjest@gmail.com>
2021-02-08 16:10:16 +0100

    Implemented entries well enough

--- src/controllers/entry.ts
+++ src/controllers/entry.ts
@@ -1,1 +1,13 @@
-export { };
+import type IEntry from "../interfaces/entry";
+import { get, post, remove, patch } from "./fetcher";
+
+
+const getAllEntries = async (): Promise<IEntry[]> => {
+	const response = await get("/entry");
+	const json = await response.json();
+
+	console.log(json);
+	return null;
+};
+
+export default { getAllEntries };
