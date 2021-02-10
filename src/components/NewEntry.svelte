9d78d2c078d14bb55222b6224ddec9863b46e487
Dan Örjestad <danorjest@gmail.com>
2021-02-10 08:37:47 +0100

    basic NewEntry structure

---
+++ src/components/NewEntry.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let description: string;
+	export let amount: number;
+</script>
+
+<input type="text" bind:value={description} />
+<input type="number" bind:value={amount} />
