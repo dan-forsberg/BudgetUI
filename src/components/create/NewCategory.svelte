5ff016da9efca2ec36dc3e1af6d6a1fcde88d68a
Dan Örjestad <danorjest@gmail.com>
2021-02-16 08:41:18 +0100

    Bug-fixes and cleanup
    API changed a bit, instead of an entrys description being "Half of gemensamma"
    it can now be "HALF_OF_GEMENSAMMA" and have a value
    The value is removed from the half of gemensamma
    to allow for evening-out in costs
    Entries are sorted automatically
    Empty entries are removed in a better way
    The total is updated when clicking outside the form instead of onChange

--- src/components/create/NewCategory.svelte
+++ src/components/create/NewCategory.svelte
@@ -1,67 +1,88 @@
 <script>
 	import gemensamTotal from './gemensamTotal';
-	import EntryRow from '../read/EntryRow.svelte';
+	import { clickOutside } from '../../clickOutside';
+	import { onMount } from 'svelte';
 
 	export let entries;
 	export let category;
 	let total = 0;
 
-	if (category === 'Dan') {
-		let halfOfGemensamma = {
-			date: new Date(),
-			amount: $gemensamTotal,
-			description: 'Hälften av gemensamma',
-			Category: entries[0].Category,
-		};
+	// special value to balance out if one person should pay more for the common costs
+	// "half of gemsamma even outer"
+	let HOG_evenOuter = 0;
+
+	// some default entries can have a special first row with description
+	// "HALF_OF_GEMENSAMMA" where the value should be the half of gemensamma's total
+	// subscribe to the total and update the amount
+	if (entries[0].description === 'HALF_OF_GEMENSAMMA') {
+		let entry = entries[0];
+		HOG_evenOuter = entry.amount;
+
+		let description = 'Halva gemensamma';
+		if (HOG_evenOuter > 0) description += ` (+${HOG_evenOuter})`;
+		else if (HOG_evenOuter < 0) description += ` (${HOG_evenOuter})`;
+		entry.description = description;
+
 		gemensamTotal.subscribe((value) => {
-			entries[0].amount = value;
+			entry.amount = value / 2 - HOG_evenOuter;
 		});
 	}
 
-	$: {
-		entries.forEach((entry) => {
-			total += parseInt(entry.amount);
-			console.log(parseInt(entry.amount));
-		});
+	const isEmptyString = (str) => {
+		return !str || str.length == 0;
+	};
 
+	// Update the total and remove any empty rows
+	// Sort the entries by amount lowest - highest
+	// Run when clicking outside the form and onMount()
+	const update = () => {
+		total = 0;
+		let emptyEntries = [];
+		entries.forEach((entry) => {
+			if (isEmptyString(entry.description) && isEmptyString(entry.amount))
+				emptyEntries.push(entry);
+			else total += parseInt(entry.amount);
+		});
 		if (category === 'Gemensamma') {
 			gemensamTotal.set(total);
 		}
 
+		entries = entries.filter((entry) => !emptyEntries.includes(entry));
+		entries.sort((a, b) => {
+			return a.amount - b.amount;
+		});
+	};
+
+	const newRow = () => {
+		entries.push({
+			category: entries[0].Category.id,
+			date: new Date(),
+			description: '',
+			amount: '',
+		});
+	};
+
+	$: {
 		let len = entries.length;
 		let last = entries[len - 1];
 		// if the last row is not empty, add an empty row
-		if (last.description !== '' && last.description !== '') {
-			// copy the category ID from the previous value, it will be correct
-			let categoryId = entries[0].Category.id;
-			entries.push({
-				category: categoryId,
-				date: new Date(),
-				description: '',
-				amount: '',
-			});
-		}
-
-		// pop the last row if the last two are empty
-		// not working if you enter something into amount, then remove it
-		if (
-			entries.length > 2 &&
-			entries[len - 1].description == '' &&
-			entries[len - 2].description == '' &&
-			entries[len - 1].amount == '' &&
-			entries[len - 2].amount == ''
-		) {
-			entries.pop();
+		if (!isEmptyString(last.description) && !isEmptyString(last.amount)) {
+			newRow();
 		}
 	}
+
+	onMount(() => {
+		update();
+	});
 </script>
 
 <h4>{category}</h4>
-<form>
+<form use:clickOutside on:click_outside={update}>
 	{#each entries as entry, index}
 		<input type="text" bind:value={entry.description} />
 		<input type="number" bind:value={entry.amount} />
 		<br />
 	{/each}
-	<EntryRow description="Total" amount={total} />
+	<input type="text" disabled value="Totalt" />
+	<input type="number" disabled bind:value={total} />
 </form>
