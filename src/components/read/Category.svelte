edf3511c555c043686b39ec635772a66b91379d9
Dan Örjestad <danorjest@gmail.com>
2021-02-23 15:17:00 +0100

    Moving the sorting of entries into the controller
    The sorting method will be reused

--- src/components/read/Category.svelte
+++ src/components/read/Category.svelte
@@ -1,37 +1,11 @@
 <script lang="ts">
 	import type IEntry from '../../interfaces/entry';
+	import { sortEntries } from '../../controllers/entry';
 
 	export let entries: IEntry[];
 	export let category: string;
 
-	/*  
-	Filter out positive from negative values, sort biggest-lowest
-	Lön 		20000 	(biggest)
-	Försäljning 100 	(lowest)
-	Hyra		-5000 	(biggest)
-	Viaplay		-109 	(lowest)
-	...
-	*/
-	let total: number = 0,
-		negative = [],
-		positive = [];
-
-	// Filter out positive values into positive, negative into negative and find the total O(n)
-	// Sort the arrays later O(n^2) if len < 10, otherwise O(n log(n))
-	entries.forEach((entry) => {
-		total += entry.amount;
-
-		if (entry.amount > 0) {
-			positive.push(entry);
-		} else {
-			negative.push(entry);
-		}
-	});
-
-	negative.sort((a, b) => a.amount - b.amount);
-	positive.sort((a, b) => b.amount - a.amount);
-
-	let sortedEntries = [...positive, ...negative];
+	const { sortedEntries, total } = sortEntries(entries);
 </script>
 
 {#if entries !== undefined}
--- src/controllers/entry.ts
+++ src/controllers/entry.ts
@@ -69,4 +69,47 @@ const newEntry = async (newEntry: IEntry | IEntry[]): Promise<IEntry> => {
 	return json;
 };
 
+/**
+ * Sort an array of entries from biggest to smallest values and get a total
+ * This sorts [-200, 500, -500, 2000] => [2000, 500, -500, 200]
+ * Used in displaying a Category and editing
+ * 
+ * @param entries An an array of unsorted entries
+ */
+const sortEntries = (entries: IEntry[]): {
+	sortedEntries: IEntry[],
+	total: number;
+} => {
+	/*  
+	Filter out positive from negative values, sort biggest-lowest
+	Lön 		20000 	(biggest)
+	Försäljning 100 	(lowest)
+	Hyra		-5000 	(biggest)
+	Viaplay		-109 	(lowest)
+	...
+	*/
+	let total = 0;
+	const negative = [], positive = [];
+
+	// Filter out positive values into positive, negative into negative and find the total O(n)
+	// Sort the arrays later O(n^2) if len < 10, otherwise O(n log(n))
+	entries.forEach((entry) => {
+		total += entry.amount;
+
+		if (entry.amount > 0) {
+			positive.push(entry);
+		} else {
+			negative.push(entry);
+		}
+	});
+
+	negative.sort((a, b) => a.amount - b.amount);
+	positive.sort((a, b) => b.amount - a.amount);
+
+	const sortedEntries = [...positive, ...negative];
+
+	return { sortedEntries, total };
+};
+
 export default { getAllEntries, getSpecificEntries, newEntry, getDefaultEntries };
+export { sortEntries };
