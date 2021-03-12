7cb0030a0173819c2099c22a16b7b4af60770164
Dan Örjestad <danorjest@gmail.com>
2021-03-12 08:57:32 +0100

    Bug fix - readOnly was sent as a string
    Now it's more guarded and shouldn't possibly be cast into a string

--- src/components/edit/EditBudget.svelte
+++ src/components/edit/EditBudget.svelte
@@ -1,5 +1,11 @@
 <script>
 import SelectBudget from "../SelectBudget.svelte";
+
+/* The reason readOnly is defined here and passed to SelectBudget is
+ * Make sure that it's a boolean and does not change anytime or anyhow
+ * If sent like `<SelectBudget readonly=false />` it /can/ be refactored to `"false"`
+ * which breaks functionality in a confusing way, because the value is false, but is a string */
+const readOnly = false;
 </script>
 
-<SelectBudget readOnly="false" />
+<SelectBudget {readOnly} />
--- src/components/read/ViewBudget.svelte
+++ src/components/read/ViewBudget.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
 import SelectBudget from "../SelectBudget.svelte";
+/* The reason readOnly is defined here and passed to SelectBudget is
+ * Make sure that it's a boolean and does not change anytime or anyhow
+ * If sent like `<SelectBudget readonly=true />` it /can/ be refactored to "true"
+ * which breaks functionality in a confusing way, because the value is true, but is a string */
+const readOnly = true;
 </script>
 
-<SelectBudget readOnly="true" />
+<SelectBudget {readOnly} />
