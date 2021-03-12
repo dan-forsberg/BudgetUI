ac33d9f911b46452c3542328bd63c13fb658b780
Dan Örjestad <danorjest@gmail.com>
2021-03-12 15:16:09 +0100

    proper paths

--- src/components/Navigator.svelte
+++ src/components/Navigator.svelte
@@ -1,12 +1,16 @@
 <script>
 let loc = window.location;
-let URL = `${loc.protocol}//${loc.hostname}${loc.pathname}`;
+let URL = `${loc.protocol}//${loc.hostname}`;
+
+if (loc.pathname.substr(0, 7) == "/budget") {
+	URL += "/budget";
+}
 </script>
 
 <nav class="indigo darken-3">
 	<div class="nav-wrapper">
 		<ul id="nav-mobile" class="left">
-			<li><a href={URL}>Hem</a></li>
+			<li><a href="{URL}/">Hem</a></li>
 			<li><a href="{URL}/new">Ny budget</a></li>
 			<li><a href="{URL}/edit">Ändra budget</a></li>
 		</ul>
