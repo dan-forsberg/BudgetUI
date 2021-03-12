ec9a6c44f2413c1229aec413d24e68dc1988a41c
Dan Örjestad <danorjest@gmail.com>
2021-03-12 09:00:11 +0100

    If IP is localhost (dev), use port 8080

--- src/controllers/fetcher.ts
+++ src/controllers/fetcher.ts
@@ -1,5 +1,11 @@
 const loc = window.location;
-const URL = `${loc.protocol}//${loc.hostname}/api`;
+let URL = `${loc.protocol}//${loc.hostname}/api`;
+
+// if dev, use port 8080
+if (loc.hostname === "0.0.0.0" || loc.hostname === "localhost" || loc.hostname === "127.0.0.1") {
+	URL = `${loc.protocol}//${loc.hostname}:8080/api`;
+}
+
 const headers = { "Content-Type": "application/json" };
 
 const httpReq = (endPoint: string, method: string, body?: unknown): Promise<Response> => {
