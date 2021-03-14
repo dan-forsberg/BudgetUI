0cfa76b9f075ab8d1e6527a5ba6e11f569db391c
Dan Örjestad <danorjest@gmail.com>
2021-03-14 15:13:23 +0100

    Testing authentication

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,13 @@
 import EditBudget from "./components/edit/EditBudget.svelte";
 import ViewBudget from "./components/read/ViewBudget.svelte";
 import NewBudget from "./components/create/NewBudget.svelte";
-
 import Navigator from "./components/Navigator.svelte";
+import { onMount } from "svelte";
 import router from "page";
+
+import createAuth0Client from "@auth0/auth0-spa-js";
+let auth0 = null;
+let isAuthenticated = false;
 
 let page = ViewBudget;
 
@@ -20,7 +24,21 @@ router("/edit", () => {
 	page = EditBudget;
 });
 
-router.start();
+onMount(async () => {
+	auth0 = await createAuth0Client({
+		domain: "dev-dasifor.eu.auth0.com",
+		client_id: "5qQ5xvpUl4gecTkaP95O1HpKhGoJMUD0",
+	});
+
+	isAuthenticated = await auth0.isAuthenticated();
+	if (!isAuthenticated) {
+		await auth0.loginWithRedirect({
+			redirect_uri: window.location.origin,
+		});
+	} else {
+		router.start();
+	}
+});
 </script>
 
 <Navigator />
