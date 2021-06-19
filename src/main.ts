e0c11b2703cb4535407dc939034e317bb10543f7
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-06-19 15:30:00 +0200

    Fixed bug that borked auth0 + minor fixes

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import "../node_modules/materialize-css/dist/css/materialize.css";
 import "../public/global.css";
 
 const app = new App({
-	target: document.body,
-	props: {
-	}
+	target: document.body
 });
 
 
