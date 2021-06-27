63761ee8c36d601a64754f58a64047837e2ec29a
Dan Örjestad <danorjest@gmail.com>
2021-06-27 10:44:35 +0200

    changed clickOutside to TS

--- src/clickOutside.ts
+++ src/clickOutside.ts
@@ -2,7 +2,7 @@
 // Taken from
 // https://svelte.dev/repl/0ace7a508bd843b798ae599940a91783?version=3.16.7
 export function clickOutside(node) {
-	const handleClick = event => {
+	const handleClick = (event: Event) => {
 		if (node && !node.contains(event.target) && !event.defaultPrevented) {
 			node.dispatchEvent(
 				new CustomEvent("click_outside", node)
